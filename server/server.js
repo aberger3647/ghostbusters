720a040cbf46210bbb3987f027a919a107106d90
JWatkins28 <112731653+JWatkins28@users.noreply.github.com>
2023-02-26 16:31:55 -0600

    added initial files

---
+++ server/server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const path = require('path');
+const db = require('./config/connection');
+// IMPORT APOLLO SERVER AND OUR TYPEDEFS/RESOLVERS
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas')
+const { authMiddleware } = require('./utils/auth')
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// CREATE THE APOLLO SERVER WITH OUR TYPEDEFS/RESOLVERS AND CONTEXT
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+// REDIRECT BAD URLS TO THE HOMEPAGE
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'))
+})
+
+// START THE APOLLO SERVER USING EXPRESS AS OUR MIDDLEWARE
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`)
+    });
+  });
+}
+
+// START THE APOLLO SERVER
+startApolloServer(typeDefs, resolvers);
