c8ce4b295ad312c6cdaa690e47b3e5a63ed23f59
Jennifer Blacutt <jennifer@blacutt.com>
2023-02-28 22:28:51 -0600

    add basic component files

---
+++ client/src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+// // import hook
+// import { useHistory } from 'react-router-dom';
+
+import Navigation from './Navigation';
+
+
+function Header() {
+
+  return (
+    <div className='header'>
+      <h1>GHOSTBUSTERS</h1>
+    </div>
+  );
+}
+
+export default Header;
+
