5aa2548734d1ed6c3c0a4a6616834438f1b6fab0
Jennifer Blacutt <jennifer@blacutt.com>
2023-03-01 11:17:58 -0600

    fix resolvers to match with schema

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -11,21 +11,19 @@ const resolvers = {
         throw new AuthenticationError('You must be logged in.');
     },
 
-    users: async (parent, { preferences }, context) => {
-        if (context.user) {
-            const users = await User.find({ $and: preferences });
-            return users;
-        }
-        throw new AuthenticationError('You must be logged in.');
+    getUserById: async (parent, { userId }, context) => {
+      if (context.user) {
+        return User.findById(userId).populate('reviews');
+      }
+      throw new AuthenticationError('You must be logged in.');
     },
 
-    matches: async (parent, args, context) => {
-        if (context.user) {
-            const matches = await Match.find({ $or: [{ user1: context.user._id }, { user2: context.user._id }] }).populate('user1 user2');
-            return matches;
-        }
-        throw new AuthenticationError('You must be logged in.');
-    },
+    getReviewsByUserId: async (parent, { userId }, context) => {
+      if (context.user) {
+        return Review.find({ reviewedUser: userId }).populate('reviewer');
+      }
+      throw new AuthenticationError('You must be logged in.');
+    }
   },
 
   Mutation: {
@@ -46,64 +44,53 @@ const resolvers = {
       return { token, user };
     },
 
-    addUser: async (parent, { username, email, password, preferences }) => {
-        const user = await User.create({ username, email, password, preferences });
+    addUser: async (parent, { email, password, fullName }) => {
+        const user = await User.create({ email, password, fullName });
         const token = signToken(user);
         return { token, user };
     },
 
-    // new review ** use input type for review data
-    // not sure how to use reviewer/reviewed
-    addReview: async (parent, { reviewedUserId, text }, context) => {
+    addReview: async (parent, { userId, reviewData }, context) => {
         if (!context.user) {
             throw new AuthenticationError('You must be logged in.');
         }
     
+        const { reviewText, username } = reviewData;
+
         const review = await Review.create({
             reviewer: context.user._id,
-            reviewedUser: reviewedUserId,
-            text,
+            reviewedUser: userId,
+            reviewText,
+            username,
         });
+
+        await User.findByIdAndUpdate(userId, { $push: { reviews: review._id } });
     
         return review;
     },
 
-    // not sure how to match another user to current user
-    addMatch: async (parent, { user2Id, preferences }, context) => {
+    // NOT SURE THIS WORKS
+    addMatch: async (parent, { userId, matchData }, context) => {
       if (context.user) {
-        const user1 = await User.findOne({ _id: context.user._id });
-        const user2 = await User.findOne({ _id: user2Id });
-
-        if (!user2) {
-          throw new Error('Cannot find user with that Id');
+          throw new AuthenticationError('User not found by that Id.');
         }
 
-        const match = await Match.create({ user1: user1._id, user2: user2._id });
+        const { email, fullName } = matchData;
+
+        const user = await User.findOne({ _id: userId });
+
+        if (!user) {
+          throw new Error('Cannot find user with that Id');
+        }
+  
+        const match = await Match.create({ user1: context.user._id, user2: user._id });
+  
+        await User.findByIdAndUpdate(userId, { $inc: { totalMatches: 1 } });
 
         return match;
       }
-      throw new AuthenticationError('You must be logged in.');
     },
-
-    // removeMatch: async (parent, { matchId }, context) => {
-    //   if (context.user) {
-    //     const match = await Match.findOne({ _id: matchId });
-
-    //     if (!match) {
-    //       throw new Error('Cannot find match with that Id');
-    //     }
-
-    //     if (!match.user1.equals(context.user._id) && !match.user2.equals(context.user._id)) {
-    //       throw new AuthenticationError('Unable to delete match.');
-    //     }
-
-    //     await Match.deleteOne({ _id: matchId });
-
-    //     return 'Match deleted';
-    //   }
-    //   throw new AuthenticationError('You must be logged in.');
-    // },
-  },
 };
+
 
 module.exports = resolvers;
