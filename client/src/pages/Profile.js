63c5a0de62b16cf47127b880ac4489115dfc71f6
Rashida Kapadia <rashidamk21@gmail.com>
2023-02-28 19:45:02 -0600

    Created profile page

---
+++ client/src/pages/Profile.js
@@ -0,0 +1,55 @@
+import React from 'react';
+
+import useQuery from '@apollo/client';
+
+const Profile = () => {
+
+    const { loading, data } = useQuery(QUERY_ME);
+
+    const user = data?.user || {};
+
+    if (!user?.userId) {
+        return (
+            <h4>
+                Sign up or login to view your profile!
+            </h4>
+        );
+    }
+
+    if (loading) {
+        return <div>Potentially app logo</div>
+    }
+
+    return (
+        <div>
+            <h2>Name</h2>
+            <p>Image</p>
+            <span>Gender, Age, Height</span>
+
+            <p>Bio</p>
+            <p>Work:</p>
+            <p>Religion:</p>
+            <p>Politics:</p>
+
+            <p>Smoking</p>
+            <p>Drinking</p>
+
+            <hr></hr>
+
+            <h4>Preferences</h4>
+
+            <p>Age:</p>
+            <p>Height</p>
+            <p>Religion</p>
+            <p>Politics:</p>
+
+            <p>Smoking</p>
+            <p>Drinking</p>
+
+            <button>Edit</button>
+            <button>Logout</button>
+        </div>
+    )
+};
+
+export default Profile;
