5de9b04637e306e57e48a19e1ada6d69f216268d
Jennifer Blacutt <jennifer@blacutt.com>
2023-03-03 00:35:52 -0600

    fix resolvers, typedefs, add seeds

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,26 +4,13 @@ const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
-    me: async (parent, args, context) => {
-        if (context.user) {
-            return User.findOne({ _id: context.user._id }).populate('savedReviews');
-        }
-        throw new AuthenticationError('You must be logged in.');
+    users: async () => {
+      return User.find();
     },
 
-    getUserById: async (parent, { userId }, context) => {
-      if (context.user) {
-        return User.findById(userId).populate('reviews');
-      }
-      throw new AuthenticationError('You must be logged in.');
+    user: async (parent, { userId }, context) => {
+      return User.findOne({ _id: userId });
     },
-
-    getReviewsByUserId: async (parent, { userId }, context) => {
-      if (context.user) {
-        return Review.find({ reviewedUser: userId }).populate('reviewer');
-      }
-      throw new AuthenticationError('You must be logged in.');
-    }
   },
 
   Mutation: {
@@ -49,48 +36,8 @@ const resolvers = {
         const token = signToken(user);
         return { token, user };
     },
-
-    addReview: async (parent, { userId, reviewData }, context) => {
-        if (!context.user) {
-            throw new AuthenticationError('You must be logged in.');
-        }
     
-        const { reviewText, username } = reviewData;
-
-        const review = await Review.create({
-            reviewer: context.user._id,
-            reviewedUser: userId,
-            reviewText,
-            username,
-        });
-
-        await User.findByIdAndUpdate(userId, { $push: { reviews: review._id } });
-    
-        return review;
-    },
-
-    // NOT SURE THIS WORKS
-    addMatch: async (parent, { userId, matchData }, context) => {
-      if (context.user) {
-          throw new AuthenticationError('User not found by that Id.');
-        }
-
-        const { email, firstName } = matchData;
-
-        const user = await User.findOne({ _id: userId });
-
-        if (!user) {
-          throw new Error('Cannot find user with that Id');
-        }
-  
-        const match = await Match.create({ user1: context.user._id, user2: user._id });
-  
-        await User.findByIdAndUpdate(userId, { $inc: { totalMatches: 1 } });
-
-        return match;
-      }
-    },
+  },
 };
-
 
 module.exports = resolvers;
