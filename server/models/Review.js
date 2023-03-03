7d6e088421eca6d2fe0b647b9093a7c312b76750
Jennifer Blacutt <jennifer@blacutt.com>
2023-03-03 16:15:08 -0600

    update to may not use

--- server/models/Review.js
+++ server/models/Review.js
@@ -1,27 +1,29 @@
-const { Schema, model } = require('mongoose');
+// just better without this for now -jb
+
+// const { Schema, model } = require('mongoose');
 
 // This is a subdocument schema, it won't become its own model but we'll use it as the schema for the User's `reviews` array in User.js
-const reviewSchema = new Schema(
-    {
-        text: {
-            type: String,
-            required: true,
-            minlength: 1,
-            maxlength: 500
-        },
-        firstName: {
-            type: String,
-            required: true
-        },
-    },
-    { timestamps: { createdAt: 'created_at'}},
-    {
-        toJSON: {
-            virtuals: true,
-            getters: true
-        },
-        id: false
-    }
-);
+// const reviewSchema = new Schema(
+//     {
+//         text: {
+//             type: String,
+//             required: true,
+//             minlength: 1,
+//             maxlength: 500
+//         },
+//         firstName: {
+//             type: String,
+//             required: true
+//         },
+//     },
+//     { timestamps: { createdAt: 'created_at'}},
+//     {
+//         toJSON: {
+//             virtuals: true,
+//             getters: true
+//         },
+//         id: false
+//     }
+// );
 
-module.exports = reviewSchema;
+// module.exports = reviewSchema;
