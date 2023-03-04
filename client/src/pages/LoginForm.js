66272558f6aeb12f5e77137dbe8e260c57d8ffd9
Rashida Kapadia <rashidamk21@gmail.com>
2023-03-04 17:05:26 -0600

    Fixed invalid token issue

--- client/src/pages/LoginForm.js
+++ client/src/pages/LoginForm.js
@@ -30,7 +30,7 @@ const LoginForm = () => {
         variables: { ...formData },
       });
       Auth.login(data.login.token);
-      console.log("logged in");
+
     } catch (err) {
       console.error(err);
     }
@@ -96,14 +96,14 @@ const LoginForm = () => {
         <>
           <form onSubmit={handleSubmit(loginSubmit)}>
             <input
-            className='loginInput'
+              className='loginInput'
               {...register("email", {
                 pattern: /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/,
               })}
               placeholder="Email Address"
             />
             <input
-            className='loginInput'
+              className='loginInput'
               type="password"
               {...register("password")}
               placeholder="Password"
