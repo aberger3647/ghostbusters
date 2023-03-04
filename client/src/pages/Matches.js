9cb71afbd35a2671800118d04c5e0257952ea6ea
Jennifer Blacutt <jennifer@blacutt.com>
2023-03-03 20:35:38 -0600

    comment out until functional

--- client/src/pages/Matches.js
+++ client/src/pages/Matches.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import { useQuery } from '@apollo/client';
-import { GET_ME } from '../queries';
+// import { useQuery } from '@apollo/client';
+// import { GET_ME } from '../queries';
+
+import MatchCard from '../components/MatchCard';
 
 const Matches = () => {
     // const { data, loading, error } = useQuery(GET_ME);
 
-    const matches = data.me.matches;
+    // const matches = data.me.matches;
 
     return (
         <div>
         <h1>My Matches</h1>
         <div className="matches">
-            {matches.map((user) => (
+            {/* {matches.map((user) => (
             <MatchCard key={user._id} user={user} />
-            ))}
+            ))} */}
         </div>
         </div>
     );
