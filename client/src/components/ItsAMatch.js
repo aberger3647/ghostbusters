80fc17415b97134cc4695dab7dd0af18571d493d
aberger3647 <112574259+aberger3647@users.noreply.github.com>
2023-03-05 21:18:59 -0600

    make profile images circular, fix firefox fill bugs

--- client/src/components/ItsAMatch.js
+++ client/src/components/ItsAMatch.js
@@ -1,36 +1,69 @@
-import React, { useState } from "react";
-import profilePhoto from "../assets/profile-icon.svg";
-import matchIcon from "../assets/its-a-match-heart.svg";
+import React, { useState, useEffect } from "react";
+import { Image, Transformation } from "cloudinary-react";
+import { GET_ME } from '../utils/queries';
+import { useQuery } from "@apollo/client";
 
 const ItsAMatch = (props) => {
   const [name, setName] = useState(["Jessica", "Marcus"]);
+  const [imageId, setImageId] = useState("");
 
+  const { loading, data } = useQuery(GET_ME);
+  console.log("data", data);
+  const me = data?.me || {};
+  const profile = data?.me.profile || {};
+
+useEffect(() => {
+    if (me) {
+        let newImage = `${me.image}.png`
+        setImageId(newImage)
+    }
+}, [me])
 
   const closeModal = () => {
-    const modal = document.querySelector('.itsAMatch');
-    modal.style.display = 'none';
+    const modal = document.querySelector(".itsAMatch");
+    modal.style.display = "none";
   };
-
 
   return (
     <div className="formContainer itsAMatch">
       <h1>it's a match!</h1>
       <h2>{name[0]}</h2>
 
-        <img
+      <Image
         className="mediumPhoto topPhoto"
-        src={profilePhoto}
-        alt="match pic"
-      />
+        cloudName={process.env.REACT_APP_CLOUD_NAME}
+        publicId={imageId}
+        alt="Match pic"
+      >
+        <Transformation
+          width="345"
+          height="345"
+          gravity="face"
+          radius="max"
+          crop="fill"
+        />
+      </Image>
 
-      <img
+      <Image
         className="mediumPhoto botPhoto"
-        src={profilePhoto}
-        alt="match pic"
-      />
+        cloudName={process.env.REACT_APP_CLOUD_NAME}
+        publicId={imageId}
+        alt='Prof pic'
+      >
+        <Transformation
+          width="345"
+          height="345"
+          gravity="face"
+          radius="max"
+          crop="fill"
+        />
+      </Image>
 
       <h2>{name[1]}</h2>
-     <button id='close' onClick={closeModal}> <h5>Close</h5></button>
+      <button id="close" onClick={closeModal}>
+        {" "}
+        <h5>Close</h5>
+      </button>
     </div>
   );
 };
