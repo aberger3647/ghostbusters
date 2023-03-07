3fc7d33e4908ff0964504e30eb636b4ee9ad5f70
JWatkins28 <112731653+JWatkins28@users.noreply.github.com>
2023-03-06 22:31:29 -0600

    fixed merge conflicts and matches page is complete

--- client/src/components/ProfileCard.js
+++ client/src/components/ProfileCard.js
@@ -9,16 +9,16 @@ const ProfileCard = (props) => {
 
     const [imageId, setImageId] = useState("");
 
-    const { loading, data } = useQuery(GET_ME);
-    const me = data?.me || {};
-    const profile = data?.me.profile || {};
+  const { loading, data } = useQuery(GET_ME);
+  const me = data?.me || {};
+  const profile = data?.me.profile || {};
 
-    useEffect(() => {
-        if (me) {
-            let newImage = `${me.image}.png`
-            setImageId(newImage)
-        }
-    }, [me])
+useEffect(() => {
+    if (me) {
+        let newImage = `${me.image}.png`
+        setImageId(newImage)
+    }
+}, [me])
 
     return (
         <>
@@ -41,7 +41,7 @@ const ProfileCard = (props) => {
             <h2 className='exploreName'>{props.name}</h2>
             <div className='exploreStatContainer'>
                 <h3 className="exploreStats">{props.gender} </h3>
-                <h3 className="exploreStats">{props.age}</h3>
+                <h3 className="exploreStats">{props.age}</h3> 
                 <h3 className="exploreStats">{props.height}</h3>
             </div>
 
